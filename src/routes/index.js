b8347046216c40ccbc79dbeb321e9d2e4a6f2ccb
hoducviet <ducvietsqt@gmail.com>
2021-07-22 21:21:25 +0700

    update layout route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import '../assets/App.css';
 import {
-  Route, Switch, Redirect,
+  Route, Switch,
 } from 'react-router-dom'
 import withLayout, { AppLayout, DefaultLayout, AuthLayout } from '../layouts'
+import Home from '../pages/home'
 import Products from '../pages/products'
 import Product from '../pages/_product'
 import SignIn from '../pages/signIn'
@@ -12,12 +13,11 @@ export default function () {
   return (
     <div>
       <Switch>
+        <Route exact path="/" component={withLayout(DefaultLayout, Home)} />
         <Route path="/auth/signin" component={withLayout(SignIn, AuthLayout)}/>
-      </Switch>
-      <Switch>
-        <Route path="/products" component={withLayout(Products, AppLayout)}/>
-        <Route path="/products/:id" component={withLayout(Product, AppLayout)} />
-        <Route path="*" component={<PageNotFound />}/>
+        <Route exact path="/products" component={withLayout(Products, AppLayout)}/>
+        <Route exact path="/products/:id" component={withLayout(Product, AppLayout)} />
+        <Route path="*" component={PageNotFound}/>
       </Switch>
     </div>
   )
